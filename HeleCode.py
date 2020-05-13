f44b0ea9e8a05f7d57ab085427cdfd5d4229a02b
Azshian <aliceralde@egi.utah.edu>
2020-05-13 17:25:42 -0600

    check

--- HeleCode.py
+++ HeleCode.py
@@ -1,16 +1,27 @@
 #Libraries
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 import tkinter as tk
-import os
 import sys
+import time
+import os
+from PIL import Image, ImageOps
+from collections import defaultdict
 
 #Set File
-file = sys.argv[1]
-isize = int(sys.argv[2]) #Do Not Change from 300
-X = np.loadtxt(file)
-print(file.readline())
-#X = np.loadtxt(file, skiprows = 4)
+file  = sys.argv[1]
+with open(file) as myfile:
+    head = [next(myfile) for x in range(5)]
+if (head[0][0:5]) == 'ascii':               #check if there's header information
+    df = pd.read_csv(file, sep = ' ', header=None, skiprows = 4)
+elif (head[0][0] == str(0) or head[0][0] ==  str(1)):
+    df = pd.read_csv(file, sep = ' ', header=None)
+df = df.iloc[:, :-1]
+print( 'File Dimensions:', df.shape[0], 'by', df.shape[1] )
+X = df.to_numpy()
+del df
+isize = len(X[0])
 
 #Extract values from slider
 def takeValues():
@@ -34,13 +45,13 @@ def showImage():
     print('Width:', '\t', '\t', width)
     print('Length:', '\t', length)
     print('')
-    plt.text(-30, 325, 'Width: ' + str(width))
-    plt.text(-30, 310, 'Length:' + str(length))
+    plt.text(-30, isize+25, 'Width: ' + str(width))
+    plt.text(-30, isize+10, 'Length:' + str(length))
 
-    #plt.hlines(y_bot,    x_left, x_right-1, 'r', linewidth = 2)
-    #plt.hlines(y_top-1,  x_left, x_right-1, 'r', linewidth = 2)
-    #plt.vlines(x_left,   y_bot,  y_top-1,   'r', linewidth = 2)
-    #plt.vlines(x_right-1,y_bot,  y_top-1,   'r', linewidth = 2)
+    plt.hlines(y_bot,    x_left, x_right-1, 'r', linewidth = 2)
+    plt.hlines(y_top-1,  x_left, x_right-1, 'r', linewidth = 2)
+    plt.vlines(x_left,   y_bot,  y_top-1,   'r', linewidth = 2)
+    plt.vlines(x_right-1,y_bot,  y_top-1,   'r', linewidth = 2)
     plt.title('Height Level:'+str(islice))
     plt.show()
 
@@ -69,8 +80,31 @@ def cutImage():
     plt.show()
 
 def saveImage():
+    print('Saving')
     plt.savefig('CroppedImage.png',bbox_inches='tight', pad_inches=0)
+    plt.close('all')
+    
+    img    = Image.open('CroppedImage.png')
+    pixels = img.load()
+    by_color = defaultdict(int)
+    for pixel in img.getdata():
+        by_color[pixel] += 1
 
+    for i in range(img.size[0]):
+            for j in range(img.size[1]):
+                    if pixels[i,j][0] == 68 and pixels[i,j][1] == 1 and pixels[i,j][2] == 84:
+                            pixels[i,j] = (0,0,0,255)
+                    elif pixels[i,j][0] == 253 and pixels[i,j][1] == 231 and pixels[i,j][2] == 36:
+                            pixels[i,j] = (255,255,255,255)
+                            
+    newimg = ImageOps.expand(img, border=1, fill='black') 
+    newimg.show()
+    imgsize = newimg.size
+    print(imgsize)
+    newimg.save("FinalImage.png")
+    os.remove('CroppedImage.png')
+
+    
 #Tinker GUI
 while 1:
     m = tk.Tk()
@@ -78,17 +112,17 @@ while 1:
     m.configure(bg='tan')
 
     stop_button = tk.Button(m, width=25, command=m.destroy, bg = 'orangered', text='Stop').pack()
-    w1 = tk.Scale(m, from_=1, to=300, length=400, orient=tk.HORIZONTAL, bg= 'beige', label='Y Top')
-    w1.set(300)
+    w1 = tk.Scale(m, from_=1, to=isize, length=400, orient=tk.HORIZONTAL, bg= 'beige', label='Y Top')
+    w1.set(isize)
     w1.pack()
-    w2 = tk.Scale(m, from_=0, to=299, length=400, orient=tk.HORIZONTAL, bg = 'beige', label='Y Bottom')
+    w2 = tk.Scale(m, from_=0, to=isize-1, length=400, orient=tk.HORIZONTAL, bg = 'beige', label='Y Bottom')
     w2.pack()
-    w3 = tk.Scale(m, from_=0, to=299, length=400, orient=tk.HORIZONTAL, bg = 'beige', label='X Left')
+    w3 = tk.Scale(m, from_=0, to=isize-1, length=400, orient=tk.HORIZONTAL, bg = 'beige', label='X Left')
     w3.pack()
-    w4 = tk.Scale(m, from_=1, to=300, length=400, orient=tk.HORIZONTAL, bg = 'beige', label='X Right')
-    w4.set(300)
+    w4 = tk.Scale(m, from_=1, to=isize, length=400, orient=tk.HORIZONTAL, bg = 'beige', label='X Right')
+    w4.set(isize)
     w4.pack()
-    w5 = tk.Scale(m, from_=0, to=298, length=400, orient=tk.HORIZONTAL, bg = 'skyblue', label='Height')
+    w5 = tk.Scale(m, from_=0, to=isize-1, length=400, orient=tk.HORIZONTAL, bg = 'skyblue', label='Height')
     w5.set(0)
     w5.pack()
     show_button    = tk.Button(m, width=15, text='Show', command=showImage, bg = 'gold').pack()
