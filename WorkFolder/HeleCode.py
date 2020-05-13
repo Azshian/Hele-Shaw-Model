33b30c2963eb3b45b89abc9fce0c7276807fd921
Azshian <adriann8399@gmail.com>
2020-05-12 23:09:33 -0700

    bruh

--- WorkFolder/HeleCode.py
+++ WorkFolder/HeleCode.py
@@ -1,35 +1,28 @@
 #Libraries
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 import tkinter as tk
-import os
 import sys
 import time
-import pandas as pd
+#from matplotlib.colors import ListedColormap
 
-start = time.time()
+#cmap = ListedColormap(['b', 'y'])
+
 
 #Set File
-file = sys.argv[1]
-isize = int(sys.argv[2]) #Do Not Change from 300
-X X
-#X = open(file, 'r')
-X = np.loadtxt(file)
-#X = pd.read_csv(file, header=None)
-#print(X.head())
-#print(X.tail())
+start = time.time()
 
-#print(file.readline())
-#X = np.loadtxt(file, skiprows = 4)
+file  = sys.argv[1]
+isize = int(sys.argv[2])
+#df = pd.read_csv(file, sep = ' ', header=None)
+df = pd.read_csv(file, sep = ' ', header=None, skiprows=4)
+df = df.iloc[:, :-1]
+X = df.to_numpy()
+del df
 
-
-print(X.ndim)
-print(X[0][0])
 elapsed_time = (time.time()-start)
 print(elapsed_time)
-
-
-
 
 
 #Extract values from slider
@@ -45,6 +38,7 @@ def showImage():
     islice  = inp[4]
 
     X2 = X[isize*islice:isize*islice+isize,:]
+    #plt.matshow(X2[0:isize, 0:isize], origin = 'lower', cmap = cmap)
     plt.matshow(X2[0:isize, 0:isize], origin = 'lower')
     plt.gca().xaxis.tick_bottom()
     
@@ -57,10 +51,10 @@ def showImage():
     plt.text(-30, 325, 'Width: ' + str(width))
     plt.text(-30, 310, 'Length:' + str(length))
 
-    #plt.hlines(y_bot,    x_left, x_right-1, 'r', linewidth = 2)
-    #plt.hlines(y_top-1,  x_left, x_right-1, 'r', linewidth = 2)
-    #plt.vlines(x_left,   y_bot,  y_top-1,   'r', linewidth = 2)
-    #plt.vlines(x_right-1,y_bot,  y_top-1,   'r', linewidth = 2)
+    plt.hlines(y_bot,    x_left, x_right-1, 'r', linewidth = 2)
+    plt.hlines(y_top-1,  x_left, x_right-1, 'r', linewidth = 2)
+    plt.vlines(x_left,   y_bot,  y_top-1,   'r', linewidth = 2)
+    plt.vlines(x_right-1,y_bot,  y_top-1,   'r', linewidth = 2)
     plt.title('Height Level:'+str(islice))
     plt.show()
 
@@ -92,28 +86,28 @@ def saveImage():
     plt.savefig('CroppedImage.png',bbox_inches='tight', pad_inches=0)
 
 #Tinker GUI
-#while 1:
-    #m = tk.Tk()
-    #m.title('Area Selection')
-    #m.configure(bg='tan')
+while 1:
+    m = tk.Tk()
+    m.title('Area Selection')
+    m.configure(bg='tan')
 
-    #stop_button = tk.Button(m, width=25, command=m.destroy, bg = 'orangered', text='Stop').pack()
-    #w1 = tk.Scale(m, from_=1, to=300, length=400, orient=tk.HORIZONTAL, bg= 'beige', label='Y Top')
-    #w1.set(300)
-    #w1.pack()
-    #w2 = tk.Scale(m, from_=0, to=299, length=400, orient=tk.HORIZONTAL, bg = 'beige', label='Y Bottom')
-    #w2.pack()
-    #w3 = tk.Scale(m, from_=0, to=299, length=400, orient=tk.HORIZONTAL, bg = 'beige', label='X Left')
-    #w3.pack()
-    #w4 = tk.Scale(m, from_=1, to=300, length=400, orient=tk.HORIZONTAL, bg = 'beige', label='X Right')
-    #w4.set(300)
-    #w4.pack()
-    #w5 = tk.Scale(m, from_=0, to=298, length=400, orient=tk.HORIZONTAL, bg = 'skyblue', label='Height')
-    #w5.set(0)
-    #w5.pack()
-    #show_button    = tk.Button(m, width=15, text='Show', command=showImage, bg = 'gold').pack()
-    #produce_button = tk.Button(m, width=15, text='Cut',  command=cutImage,  bg = 'gold').pack()
-    #save_button    = tk.Button(m, width=15, text='Save', command=saveImage, bg = 'gold').pack()
+    stop_button = tk.Button(m, width=25, command=m.destroy, bg = 'orangered', text='Stop').pack()
+    w1 = tk.Scale(m, from_=1, to=300, length=400, orient=tk.HORIZONTAL, bg= 'beige', label='Y Top')
+    w1.set(300)
+    w1.pack()
+    w2 = tk.Scale(m, from_=0, to=299, length=400, orient=tk.HORIZONTAL, bg = 'beige', label='Y Bottom')
+    w2.pack()
+    w3 = tk.Scale(m, from_=0, to=299, length=400, orient=tk.HORIZONTAL, bg = 'beige', label='X Left')
+    w3.pack()
+    w4 = tk.Scale(m, from_=1, to=300, length=400, orient=tk.HORIZONTAL, bg = 'beige', label='X Right')
+    w4.set(300)
+    w4.pack()
+    w5 = tk.Scale(m, from_=0, to=299, length=400, orient=tk.HORIZONTAL, bg = 'skyblue', label='Height')
+    w5.set(0)
+    w5.pack()
+    show_button    = tk.Button(m, width=15, text='Show', command=showImage, bg = 'gold').pack()
+    produce_button = tk.Button(m, width=15, text='Cut',  command=cutImage,  bg = 'gold').pack()
+    save_button    = tk.Button(m, width=15, text='Save', command=saveImage, bg = 'gold').pack()
     
-    #m.mainloop()
-    #break
+    m.mainloop()
+    break
